abbbfc5f95ffd70539b989484c9e3afe7254245f
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2021-10-18 17:39:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  console.log("yo,yo");
+  return (
+    <div className="App">
+      <h1>Hello Code Nation!!</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <marquee> weee </marquee>
+      <p> hi hello hi </p>
+    </div>
+  );
+}
