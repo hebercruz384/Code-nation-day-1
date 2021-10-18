cce33b8a08ec6ab537e0db94208d5008894c9212
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2021-10-18 17:59:22 -0400

    Add Image of South Park Superheroes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ export default function App() {
     <div className="App">
       <h1>Hello Heber Cruz </h1>
       <h2>Year 2021-2022 : John Jay School For Law HS </h2>
-      <marquee> weee </marquee>
+      <marquee> I'm swiiimmmiiinggg </marquee>
       <p> hi hello hi </p>
+      <img src="https://tv-fanatic-res.cloudinary.com/iu/s--OgcMlbjX--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1371215599/mysterion-rises-pic.png" />
     </div>
   );
 }
